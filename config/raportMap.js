6844ea3ef4fd64058802f6ba7de6deef1a25fd20
Muhammadfahril585 <laodefahril00@gmail.com>
2026-01-24 22:46:41 +0800

    Create raportMap.js

---
+++ config/raportMap.js
@@ -0,0 +1,35 @@
+module.exports = {
+  tahfidz: {
+    s1: {
+      target: "B10", januari: "C10", februari: "D10", maret: "E10", april: "F10", mei: "G10",
+      total_semester: "H10", total_keseluruhan: "I10", keterangan: "J10",
+      hifdz: "C14", tajwid: "C15", jumlah_hafalan: "D14", juz_diuji: "F14", ket_quran: "H14",
+      kepribadian: [
+        { nilai: "C19", predikat: "E19", deskripsi: "G19" },
+        { nilai: "C20", predikat: "E20", deskripsi: "G20" },
+        { nilai: "C21", predikat: "E21", deskripsi: "G21" },
+        { nilai: "C22", predikat: "E22", deskripsi: "G22" },
+        { nilai: "C23", predikat: "E23", deskripsi: "G23" }
+      ],
+      catatan: "A29:J32", tanggal: "I37",
+      pengembangan_diri: [{ nilai: "C26" }, { nilai: "C27" }],
+      ttd: { orang_tua: "B39", pimpinan: "E43", muhafidz: "I43" }
+    },
+    s2: {
+      target: "M10", juli: "N10", agustus: "O10", september: "P10", oktober: "Q10", november: "R10",
+      total_semester: "S10", total_keseluruhan: "T10", keterangan: "U10",
+      hifdz: "N14", tajwid: "N15", jumlah_hafalan: "O14", juz_diuji: "Q14", ket_quran: "S14",
+      kepribadian: [
+        { nilai: "N19", predikat: "P19", deskripsi: "R19" },
+        { nilai: "N20", predikat: "P20", deskripsi: "R20" },
+        { nilai: "N21", predikat: "P21", deskripsi: "R21" },
+        { nilai: "N22", predikat: "P22", deskripsi: "R22" },
+        { nilai: "N23", predikat: "P23", deskripsi: "R23" }
+      ],
+      catatan: "L29:U32", tanggal: "T37",
+      pengembangan_diri: [{ nilai: "N26" }, { nilai: "N27" }],
+      ttd: { orang_tua: "M39", pimpinan: "P43", muhafidz: "T43" }
+    }
+  }
+};
+
